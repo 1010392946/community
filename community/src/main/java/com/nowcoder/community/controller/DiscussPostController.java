3299cf3a380fd04da0493124c31f81af59b2e05d
1010392946 <1010392946@qq.com>
2023-05-30 16:16:01 +0800

    5.30

--- community/src/main/java/com/nowcoder/community/controller/DiscussPostController.java
+++ community/src/main/java/com/nowcoder/community/controller/DiscussPostController.java
@@ -3,14 +3,13 @@ package com.nowcoder.community.controller;
 import com.nowcoder.community.entity.DiscussPost;
 import com.nowcoder.community.entity.User;
 import com.nowcoder.community.service.DiscussPostService;
+import com.nowcoder.community.service.UserService;
 import com.nowcoder.community.util.CommunityUtil;
 import com.nowcoder.community.util.HostHolder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
 
@@ -33,6 +32,9 @@ public class DiscussPostController {
     @Autowired
     private HostHolder hostHolder;
 
+    @Autowired
+    private UserService userService;
+
     @RequestMapping(path = "/add", method = RequestMethod.POST)
     @ResponseBody
     public String addDiscussPost(String title, String content){
@@ -49,5 +51,17 @@ public class DiscussPostController {
 
         //报错的情况，将来统一处理
         return CommunityUtil.getJSONString(0,"发布成功");
+    }
+
+    @RequestMapping(path = "/detail/{discussPostId}", method = RequestMethod.GET)
+    public String getDiscussPost(@PathVariable("discussPostId") int disscussPostId, Model model){
+        //帖子
+        DiscussPost post = discussPostService.findDiscussPostById(disscussPostId);
+        model.addAttribute("post", post);
+        //查询贴子的作者
+        User user = userService.findUserById(post.getUserId());
+        model.addAttribute("user", user);
+
+        return "/site/discuss-detail";
     }
 }
